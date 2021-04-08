01eeafe732c57947839bdb9165ec8c540290a71e
Niklas <niklasbg@live.dk>
2021-04-08 20:07:00 +0200

    Started on #7

---
+++ 7/7/Parser.fs
@@ -0,0 +1,104 @@
+﻿module ImpParser
+
+    open Eval
+
+    (*
+
+    The interfaces for JParsec and FParsecLight are identical and the implementations should always produce the same output
+    for successful parses although running times and error messages will differ. Please report any inconsistencies.
+
+    *)
+
+    open JParsec.TextParser             // Example parser combinator library. Use for CodeJudge.
+    // open FParsecLight.TextParser     // Industrial parser-combinator library. Use for Scrabble Project.
+    
+    let pIntToChar  = pstring "intToChar"
+    let pPointValue = pstring "pointValue"
+
+    let pCharToInt  = pstring "charToInt"
+    let pToUpper    = pstring "toUpper"
+    let pToLower    = pstring "toLower"
+    let pCharValue  = pstring "charValue"
+
+    let pTrue       = pstring "true"
+    let pFalse      = pstring "false"
+    let pIsDigit    = pstring "isDigit"
+    let pIsLetter   = pstring "isLetter"
+
+    let pif       = pstring "if"
+    let pthen     = pstring "then"
+    let pelse     = pstring "else"
+    let pwhile    = pstring "while"
+    let pdo       = pstring "do"
+    let pdeclare  = pstring "declare"
+
+    let whitespaceChar = satisfy (fun x -> System.Char.IsWhiteSpace x)
+    let pletter        = satisfy (fun x -> System.Char.IsLetter x)
+    let palphanumeric  = satisfy (fun x -> System.Char.IsLetterOrDigit x)
+
+    let spaces         = many (pstring " ")
+    let spaces1        = many1 (pstring " ")
+
+    let (.>*>.) (p1 : Parser<'a>) (p2 : Parser<'b>) = p1 .>> spaces .>>. p2
+    let (.>*>) (p1 : Parser<'a>) (p2 : Parser<'b>)  = p1 .>> spaces .>> p2
+    let (>*>.) (p1 : Parser<'a>) (p2 : Parser<'b>)  = p1 .>> spaces >>. p2
+
+    let parenthesise (p : Parser<'a>) = (((pstring "(" >>. spaces) >>. p) .>> spaces) .>> pstring ")"
+
+    let pid = pstring "not implemented"
+
+    
+    let unop _ = failwith "not implemented"
+    let binop _ p1 p2 = p1 .>>. p2 // incorrect (not implemented)
+
+    let TermParse, tref = createParserForwardedToRef<aExp>()
+    let ProdParse, pref = createParserForwardedToRef<aExp>()
+    let AtomParse, aref = createParserForwardedToRef<aExp>()
+
+    let AddParse = binop (pchar '+') ProdParse TermParse |>> Add <?> "Add"
+    do tref := choice [AddParse; ProdParse]
+
+    let MulParse = binop (pchar '*') AtomParse ProdParse |>> Mul <?> "Mul"
+    do pref := choice [MulParse; AtomParse]
+
+    let NParse   = pint32 |>> N <?> "Int"
+    let ParParse = parenthesise TermParse
+    do aref := choice [NParse; ParParse]
+
+    let AexpParse = TermParse 
+
+    let CexpParse = pstring "not implemented"
+
+    let BexpParse = pstring "not implemented"
+
+    let stmntParse = pstring "not implemented"
+
+(* These five types will move out of this file once you start working on the project *)
+    type coord      = int * int
+    type squareProg = Map<int, string>
+    type boardProg  = {
+            prog       : string;
+            squares    : Map<int, squareProg>
+            usedSquare : int
+            center     : coord
+    
+            isInfinite : bool   // For pretty-printing purposes only
+            ppSquare   : string // For pretty-printing purposes only
+        }
+
+    type word   = (char * int) list
+    type square = Map<int, word -> int -> int -> int>
+
+    let parseSquareFun _ = failwith "not implemented"
+
+    let parseBoardFun _ = failwith "not implemented"
+
+    type boardFun = coord -> square option
+    type board = {
+        center        : coord
+        defaultSquare : square
+        squares       : boardFun
+    }
+
+    let parseBoardProg (bp : boardProg) = failwith "not implemented"
+
